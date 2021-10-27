0af29f03e4c9dfd025a21fe5f4ae85e7fe6d304c
vvp1488 <drozdik24011994@gmail.com>
2021-10-27 22:45:51 +0300

    added test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Message
 
-# Create your tests here.
+
+class MessagesTests(APITestCase):
+    def test_new_message(self):
+
+        url = reverse('new_message')
+        data = {'Author': 'Гость', 'email': 'test@ukr.net', 'text': 'adasdas'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Message.objects.count(), 1)
+        self.assertEqual(Message.objects.get().email, 'test@ukr.net')
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import MessageList, MessageDetail, NewMessage
 
 urlpatterns = [
-    path('messages/list/<int:page>/', MessageList.as_view()),
+    path('messages/list/<int:page>/', MessageList.as_view(), name='messages_list'),
     path('messages/single/<int:id>/', MessageDetail.as_view()),
-    path('newmessage/', NewMessage.as_view()),
+    path('newmessage/', NewMessage.as_view(), name='new_message'),
 ]
